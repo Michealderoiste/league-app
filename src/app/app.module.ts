5fa07b12e1ab4e3b143f0ac2afef8fca91685d0f
Mike <mike@traveldepartment.ie>
2018-02-28 14:12:40 +0000

    structure of site

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {LeagueComponent} from './league/league.component';
+import {TeamsComponent} from './league/teams/teams.component';
+import {PlayersComponent} from './league/teams/players/players.component';
+import {TablesComponent} from './league/tables/tables.component';
 
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        LeagueComponent,
+        TeamsComponent,
+        PlayersComponent,
+        TablesComponent
+    ],
+    imports: [
+        BrowserModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
